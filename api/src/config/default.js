648f2f24150e2179060ba69863af1b5b6876c1ba
Pavel Savchenko <asfaltboy@gmail.com>
2020-05-03 11:55:49 +0200

    feat: add db to development docker-compose
    Instead of using the hacky host.docker.internal interface, which
    depends on a DB to be set up by the developer on their machine.

--- api/src/config/default.js
+++ api/src/config/default.js
@@ -2,12 +2,12 @@ module.exports = {
     jwtSecret: "secret",
     sessionSecret: "topsecret",
     postgres: {
-        host: "localhost",
-        port: "5432",
-        username: "postgres",
-        password: "root",
-        database: "corona",
-        readReplicaHost: "localhost"
+        host: process.env.DB_HOST || "localhost",
+        port: process.env.DB_PORT || "5432",
+        username: process.env.DB_USERNAME || "postgres",
+        password: process.env.DB_PASSWORD || "root",
+        database: process.env.DB_DATABASE || "corona",
+        readReplicaHost: process.env.DB_READ_REPLICA_HOST || "localhost"
     },
     apiToken: "token"
 };
